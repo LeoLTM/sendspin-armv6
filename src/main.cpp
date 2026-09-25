a8a07b461af560bed30d938fae66a0dde8309d68
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2026-09-25 21:58:15 +0200

    chore: update project version to 0.1.9 in CMakeLists and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 using namespace sendspin;
 
 static constexpr const char* DEFAULT_CONFIG_PATH = "/etc/sendspin-armv6.conf";
-static constexpr const char* VERSION = "0.1.8";
+static constexpr const char* VERSION = "0.1.9";
 
 static std::atomic<bool> running{true};
 
